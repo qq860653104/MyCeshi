324abe528799a29a3f01a1dfe9aadf34793f93e9
nb74110 <qq860653104>
2019-10-22 13:26:19 +0800

    Initial commit

---
+++ xiaoxiangketanglibrary/src/main/java/com/chunleikeji/xiaoxiangketanglibrary/MyCeshi.java
@@ -0,0 +1,11 @@
+package com.chunleikeji.xiaoxiangketanglibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MyCeshi {
+    public static void showToast(Context context , String me){
+        Toast.makeText(context,me,Toast.LENGTH_LONG).show();
+
+    }
+}
